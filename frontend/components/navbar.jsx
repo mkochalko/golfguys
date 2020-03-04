3bf16210a7c8949ff51f857bbfaf6c5c7875c818
mkochalko <matthew.kochalko@gmail.com>
2020-03-03 20:19:46 -0800

    Setup mobile nav click

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.showMobileNav = this.showMobileNav.bind(this);
+    }
+
+    showMobileNav() {
+        const mobileNav = document.querySelector(".navbar-ul-mobile")
+        mobileNav.classList.toggle('mobile-navbar-hidden')
+        // mobileNav.style.display = 'flex';
+    }
 
     render() {
 
@@ -14,8 +25,8 @@ class Navbar extends React.Component {
                     <li><a href="https://github.com/mkochalko" target="_blank">GitHub</a></li>
                     <li><Link to="/careers">Careers</Link></li>
                 </ul>
-                <button></button>
-                <ul className="navbar-ul-mobile">
+                <button onClick={this.showMobileNav} className="navbar-button-mobile"><img src="https://github.com/mkochalko/golfguys/blob/master/app/assets/images/navbar_image.png?raw=true" /></button>
+                <ul className="navbar-ul-mobile mobile-navbar-hidden">
                     <li><Link to="/about">About</Link></li>
                     <li><a href="https://www.linkedin.com/in/mkochalko/" target="_blank">LinkedIn</a></li>
                     <li><a href="https://github.com/mkochalko" target="_blank">GitHub</a></li>
