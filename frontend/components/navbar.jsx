75468535bc4d7e1de056c647d25963d550d6115c
mkochalko <matthew.kochalko@gmail.com>
2020-03-03 21:30:48 -0800

    Start mobile navbar animation

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -9,9 +9,8 @@ class Navbar extends React.Component {
     }
 
     showMobileNav() {
-        const mobileNav = document.querySelector(".navbar-ul-mobile")
+        const mobileNav = document.querySelector(".navbar-ul-container")
         mobileNav.classList.toggle('mobile-navbar-hidden')
-        // mobileNav.style.display = 'flex';
     }
 
     render() {
@@ -26,12 +25,15 @@ class Navbar extends React.Component {
                     <li><Link to="/careers">Careers</Link></li>
                 </ul>
                 <button onClick={this.showMobileNav} className="navbar-button-mobile"><img src="https://github.com/mkochalko/golfguys/blob/master/app/assets/images/navbar_image.png?raw=true" /></button>
-                <ul className="navbar-ul-mobile mobile-navbar-hidden">
-                    <li><Link to="/about">About</Link></li>
-                    <li><a href="https://www.linkedin.com/in/mkochalko/" target="_blank">LinkedIn</a></li>
-                    <li><a href="https://github.com/mkochalko" target="_blank">GitHub</a></li>
-                    <li><Link to="/careers">Careers</Link></li>
-                </ul>
+                <div className='navbar-ul-container mobile-navbar-hidden'>
+                    <button onClick={this.showMobileNav} className="navbar-button-mobile"><img src="https://github.com/mkochalko/golfguys/blob/master/app/assets/images/navbar_image.png?raw=true" /></button>
+                    <ul className="navbar-ul-mobile">
+                        <li><Link to="/about">About</Link></li>
+                        <li><a href="https://www.linkedin.com/in/mkochalko/" target="_blank">LinkedIn</a></li>
+                        <li><a href="https://github.com/mkochalko" target="_blank">GitHub</a></li>
+                        <li><Link to="/careers">Careers</Link></li>
+                    </ul>
+                </div>
             </div>
         )
     }
