48e314b2f4ec879019a446069599a19a63c55f9a
mkochalko <matthew.kochalko@gmail.com>
2020-03-21 15:45:56 -0700

    Setup Routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,7 @@
 class Api::UsersController < ApplicationController
+    def index
+
+    end
+
+    def 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#root'
 
-  
+  namespace :api, defaults: { format: :json } do 
+
+    resources :users, only: [:index, :show, :create]
+    resources :players, only: [:index, :show, :create]
+    
+  end
 end
