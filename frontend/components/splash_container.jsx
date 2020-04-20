ca30e1a9b579bfe3e59910974dba8df25fdebb36
mkochalko <matthew.kochalko@gmail.com>
2020-04-20 14:06:31 -0700

    Setup leaderboard toggle

--- frontend/components/splash_container.jsx
+++ frontend/components/splash_container.jsx
@@ -6,24 +6,48 @@ class SplashContainer extends React.Component {
     constructor(props) {
         super(props)
 
-
+        this.toggleScores = this.toggleScores.bind(this)
     }
 
-    toggleScores() {
+    componentDidMount() {
+        let golfGuysButton = document.querySelector(".golf-guys-button")
+        golfGuysButton.disabled = true;
+    }
 
+    toggleScores(e) {
+        console.log(e.currentTarget.innerHTML)
+        let tab = e.currentTarget.innerHTML;
+        let golfGuys = document.getElementById("golf-guys")
+        let masters = document.getElementById("masters")
+        let golfGuysButton = document.querySelector(".golf-guys-button")
+        let mastersButton = document.querySelector(".masters-button")
+        if (tab === 'Golf Guys') {
+            golfGuysButton.disabled = true;
+            mastersButton.disabled = false;
+        } else {
+            golfGuysButton.disabled = false;
+            mastersButton.disabled = true;
+
+        }
+        golfGuys.classList.toggle('disable')
+        masters.classList.toggle('disable')
     }
 
     render() {
 
         return (
             <div className="body-container">
-                <div>
-                    <button>Masters</button>
-                    <TournamentContainer />
+                <div className='leaderboard-buttons'>
+                    <button className="masters-button" onClick={this.toggleScores}>Masters</button>
+                    <button className="golf-guys-button" onClick={this.toggleScores}>Golf Guys</button>
                 </div>
-                <div>
-                    <button>Golf Guys</button>
-                    <GameContainer />
+                <div className='leaderboards'>
+                    <div id="masters" className="disable">
+                        <TournamentContainer />
+                    </div>
+                    <div id="golf-guys">
+                        <GameContainer />
+                    </div>
                 </div>
             </div>
         )
